97e458555d89910f78579e32e7bdb16d6998c7ae
DeibyKwl <75148109+DeibyKwl@users.noreply.github.com>
2023-11-30 16:15:31 -0500

    Update testing_flask.py

--- testing_flask.py
+++ testing_flask.py
@@ -12,8 +12,24 @@ def index():
 
 config_file = "connectorConfig.json"
 
-@app.route('/test')
-def test_query():
+@app.route('/all_stores')
+def all_stores_query():
+    with open(config_file, "r") as f:
+        config = json.load(f)
+    connection_config = config["mysql"]
+    data_base = mysql.connector.connect(**connection_config)
+
+    # preparing a cursor object
+    cur = data_base.cursor()
+    cur.execute("SELECT * FROM store")
+    data = cur.fetchall()
+    cur.close()
+    return jsonify({'store': data})
+
+
+
+@app.route('/all_games')
+def all_games_query():
     with open(config_file, "r") as f:
         config = json.load(f)
     connection_config = config["mysql"]
@@ -24,15 +40,8 @@ def test_query():
     cur.execute("SELECT * FROM games")
     data = cur.fetchall()
     cur.close()
-    #return str(data)
     return jsonify({'games': data})
 
-
-    # cur = mysql.connection.cursor()
-    # cur.execute("SELECT * FROM games")
-    # data = cur.fetchall()
-    # cur.close()
-    #return str(data)
 
 
 if __name__ == '__main__':
