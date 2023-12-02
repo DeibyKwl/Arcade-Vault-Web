57ea4ae6a289a62889756995d04fcd0c3397319a
alexo75 <alexander.oneill75@gmail.com>
2023-12-01 22:49:01 -0500

    shiny logo :sparkles:

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -28,23 +28,29 @@ const HomePage = () => {
     <div className="vaporwave-bg min-h-screen flex flex-col">
       {/* Navigation at the top */}
       <nav className="flex justify-center space-x-4 p-4">
-        
+
         <h1 className={`${activeTab === 'stores' ? 'active-tab' : ''}`}>
           <button onClick={() => handleTabClick('stores')}>All Stores</button>
         </h1>
-        
+
         <h1 className={`${activeTab === 'games' ? 'active-tab' : ''}`}>
           <button onClick={() => handleTabClick('games')}>All Games</button>
         </h1>
-        
+
         <h1 className={`${activeTab === 'gamesbyyear' ? 'active-tab' : ''}`}>
-          <button onClick={() => handleTabClick('gamesbyyear')}>Games By Year |</button>
+          <button onClick={() => handleTabClick('gamesbyyear')}>Games By Year</button>
         </h1>
 
         <h1 className={`${activeTab === 'gamesbygenre' ? 'active-tab' : ''}`}>
           <button onClick={() => handleTabClick('gamesbygenre')}>Games By Genre</button>
         </h1>
       </nav>
+
+      {/* Main content */}
+      {/* I'd like to center this */}
+      <div className="text-center">
+        <h1 className="text-teal-600 text text-2xl outline-text">Arcade Vault</h1>
+      </div>
 
       <div className="flex flex-col flex-grow items-center justify-start pt-8">
         <div className="w-full md:w-2/3 lg:w-1/3 bg-black text-white p-4 rounded-lg shadow-lg mx-auto">
